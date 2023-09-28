156a1e2a1e0d0a489308ee4d20fdbe1fc4ef58ae
Hyno03 <jenny2117@naver.com>
2023-09-29 01:28:59 +0900

    Follow handprint
    character follow handprint
    but character's footstep don't disappear

--- move_character_with_mouse.py
+++ move_character_with_mouse.py
@@ -8,10 +8,10 @@ TUK_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
 hand = load_image('hand_arrow.png')
 
-def random_handprint():
-    random_x = random.randint(0, TUK_WIDTH)
-    random_y = random.randint(0, TUK_HEIGHT)
-    hand.draw(random_x,random_y)
+# def random_handprint():
+#     random_x = random.randint(0, TUK_WIDTH)
+#     random_y = random.randint(0, TUK_HEIGHT)
+#     hand.draw(random_x,random_y)
 
 def handle_events():
     global running
@@ -23,7 +23,7 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
-def draw_line(p1, p2):
+def move_in_line(p1, p2):
     x1, y1 = p1[0], p1[1]
     x2, y2 = p2[0], p2[1]
 
@@ -31,20 +31,24 @@ def draw_line(p1, p2):
         t = i / 100
         x = (1-t)*x1 + t*x2
         y = (1-t)*y1 + t*y2
+        character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+
 
 running = True
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 frame = 0
+random_x = random.randint(0, TUK_WIDTH)
+random_y = random.randint(0, TUK_HEIGHT)
 
 while running:
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
-    random_handprint()
+    # character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    hand.draw(random_x,random_y)
+    move_in_line((x,y),(random_x,random_y))
     update_canvas()
     frame = (frame + 1) % 8
     handle_events()
-    delay(0.1)
 
 
 close_canvas()
