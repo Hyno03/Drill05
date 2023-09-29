675c19f3ac7da5ce5365f2e17797e684a64d2fda
Hyno03 <jenny2117@naver.com>
2023-09-29 15:26:25 +0900

    Character move to handprint once
    modify character move to handprint once time

--- move_character_with_mouse.py
+++ move_character_with_mouse.py
@@ -9,6 +9,8 @@ character = load_image('animation_sheet.png')
 hand = load_image('hand_arrow.png')
 
 # def random_handprint():
+#     global  random_x, random_y
+#
 #     random_x = random.randint(0, TUK_WIDTH)
 #     random_y = random.randint(0, TUK_HEIGHT)
 #     hand.draw(random_x,random_y)
@@ -26,14 +28,16 @@ def move_in_line():
     global player_first_x, player_first_y
     global player_last_x, player_last_y
     global x, y
-    global i
+    global i, once
 
-    t = i / 100
-    x = (1-t)*player_first_x + t*player_last_x
-    y = (1-t)*player_first_y + t*player_last_y
-    i += 1
-    if i > 100:
-        i = 0
+    if once < 1:
+        t = i / 100
+        x = (1-t)*player_first_x + t*player_last_x
+        y = (1-t)*player_first_y + t*player_last_y
+        i += 1
+        if i > 100:
+            i = 0
+            once += 1
 
 
 running = True
@@ -43,18 +47,20 @@ x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 player_first_x, player_first_y = x, y
 player_last_x, player_last_y = random_x, random_y
 frame = 0
-i = 0
+i, once = 0, 0
 
 
 while running:
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
     character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
-    hand.draw(random_x, random_y)
+    hand.draw(random_x,random_y)
     move_in_line()
     update_canvas()
     frame = (frame + 1) % 8
+
     handle_events()
+    delay(0.01)
 
 
 close_canvas()
