6a93e601470417b5c17a5d3f0cbe289d6da11b00
Hyno03 <jenny2117@naver.com>
2023-09-29 15:21:18 +0900

    Modify code follow handprint
    code it to show character move to handprint without footsteps

--- move_character_with_mouse.py
+++ move_character_with_mouse.py
@@ -15,7 +15,6 @@ hand = load_image('hand_arrow.png')
 
 def handle_events():
     global running
-    global x, y
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -23,29 +22,36 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
-def move_in_line(p1, p2):
-    x1, y1 = p1[0], p1[1]
-    x2, y2 = p2[0], p2[1]
+def move_in_line():
+    global player_first_x, player_first_y
+    global player_last_x, player_last_y
+    global x, y
+    global i
 
-    for i in range(0, 100+1, 4):
-        t = i / 100
-        x = (1-t)*x1 + t*x2
-        y = (1-t)*y1 + t*y2
-        character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    t = i / 100
+    x = (1-t)*player_first_x + t*player_last_x
+    y = (1-t)*player_first_y + t*player_last_y
+    i += 1
+    if i > 100:
+        i = 0
 
 
 running = True
-x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
-frame = 0
 random_x = random.randint(0, TUK_WIDTH)
 random_y = random.randint(0, TUK_HEIGHT)
+x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+player_first_x, player_first_y = x, y
+player_last_x, player_last_y = random_x, random_y
+frame = 0
+i = 0
+
 
 while running:
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    # character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
-    hand.draw(random_x,random_y)
-    move_in_line((x,y),(random_x,random_y))
+    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    hand.draw(random_x, random_y)
+    move_in_line()
     update_canvas()
     frame = (frame + 1) % 8
     handle_events()
